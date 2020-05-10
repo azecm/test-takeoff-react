bfe66e7f5c3951b11a5c13fbac436fb25650a3ba
azecm <azecm@mail.ru>
2020-05-10 09:57:12 +0300

    init

---
+++ precache-manifest.684bfda505916c0c636b8836518bd38c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdc4a049db9ac726050f4b0a8a426fb6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2cc019cd0fa73b404170",
+    "url": "./static/css/main.278953e2.chunk.css"
+  },
+  {
+    "revision": "6c68008fae793d03a559",
+    "url": "./static/js/2.e5201a03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.e5201a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc019cd0fa73b404170",
+    "url": "./static/js/main.9fdf5a82.chunk.js"
+  },
+  {
+    "revision": "dc8fe1c63236b4831acd",
+    "url": "./static/js/runtime-main.28729f15.js"
+  },
+  {
+    "revision": "a0920369837035de84583e8ee5ed30c8",
+    "url": "./static/media/photo.a0920369.jpg"
+  }
+]);
